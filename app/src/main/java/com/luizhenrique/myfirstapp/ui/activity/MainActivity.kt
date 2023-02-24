bfd7a4e34181e7bb95a874afdf9cadaa4cace76d
Luiz Henrique <luizbtc13@gmail.com>
2023-02-23 22:12:54 -0300

    Layout Learning

--- app/src/main/java/com/luizhenrique/myfirstapp/ui/activity/MainActivity.kt
+++ app/src/main/java/com/luizhenrique/myfirstapp/ui/activity/MainActivity.kt
@@ -3,10 +3,15 @@ package com.luizhenrique.myfirstapp.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.luizhenrique.myfirstapp.R
+import com.luizhenrique.myfirstapp.ui.fragment.categories.CategoriesFragment
 
 class MainActivity : AppCompatActivity() {
+    private val manager = supportFragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        manager.beginTransaction().add(R.id.container_fragment, CategoriesFragment()).commit()
     }
 }
--- app/src/main/java/com/luizhenrique/myfirstapp/ui/fragment/categories/CategoriesFragment.kt
+++ app/src/main/java/com/luizhenrique/myfirstapp/ui/fragment/categories/CategoriesFragment.kt
@@ -1,7 +1,27 @@
 package com.luizhenrique.myfirstapp.ui.fragment.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.luizhenrique.myfirstapp.R
+import com.luizhenrique.myfirstapp.ui.activity.MainActivity
 
 class CategoriesFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).title = "Principal - Categorias"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_categories, container, false)
+        return root
+    }
 
 }
